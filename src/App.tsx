a69a8bf6597306a9664364cdebe31465f7ac999c
Rafael Valverde <rafaverde@msn.com>
2025-04-02 18:28:53 -0300

    Add Radix UI components and enhance App layout with new input and button elements. Home page layout created.

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,84 @@
+import { BookMarked, Link, Search, UsersRound } from "lucide-react";
+import { Button } from "./components/ui/button";
+import { Input } from "./components/ui/input";
+
 export default function App() {
   return (
-    <div className="font-primary flex h-dvh flex-col items-center justify-center gap-5 bg-zinc-900 px-4">
-      <form className="w-[600px] rounded-2xl border border-zinc-700 bg-zinc-800 p-5 shadow-2xl">
-        <header className="flex flex-col items-center">
+    <div className="font-primary bg-zinc-900 px-5">
+      <div className="m-auto flex h-dvh max-w-[600px] flex-col items-center justify-center gap-5">
+        <form className="rounded-2xl border border-zinc-700 bg-zinc-800 p-6">
+          <header className="flex flex-col items-center">
+            <img
+              src="./src/assets/github-profile-logo.svg"
+              alt="Github Profile App Logo"
+              width={250}
+            />
+            <p className="mt-4 w-10/12 text-center text-zinc-400">
+              Bem-vindo ao buscador de perfis no github. Digite o nome do
+              usuário e veja o perfil que deseja.
+            </p>
+          </header>
+
+          <div className="focus-within:shadow-accent focus-within:ring-accent mt-5 flex items-center justify-center rounded-2xl border border-zinc-700 bg-zinc-900 px-4 py-2 focus-within:outline-2 focus-within:outline-emerald-600">
+            <span className="text-sm text-emerald-400">github.com/</span>
+            <Input
+              type="text"
+              placeholder="username"
+              className="border-0 pl-0.5 text-white placeholder:text-zinc-500 focus:border-0 focus:outline-0 focus-visible:border-0 focus-visible:ring-0"
+            />
+            <Button
+              type="submit"
+              className="cursor-pointer bg-emerald-700 hover:bg-emerald-500"
+            >
+              <Search />
+            </Button>
+          </div>
+        </form>
+        <div
+          id="profile"
+          className="flex flex-col gap-6 rounded-2xl border border-zinc-700 bg-zinc-800 p-6 lg:flex-row"
+        >
           <img
-            src="./src/assets/github-profile-logo.svg"
-            alt="Github Profile App Logo"
-            width={250}
+            src="https://www.github.com/rafaverde.png"
+            alt="Profile Avatar"
+            className="block h-20 w-20 rounded-full border-4 border-emerald-700"
+            width={80}
+            height={80}
           />
-          <p className="w-10/12 text-center text-zinc-400">
-            Bem-vindo ao buscador de perfis no github. Digite o nome do usuário
-            e veja o perfil que deseja.
-          </p>
-        </header>
-      </form>
 
-      <div
-        id="profile"
-        className="flex w-[600px] gap-6 rounded-2xl border border-zinc-700 bg-zinc-800 p-5 shadow-2xl"
-      >
-        <img
-          src="https://www.github.com/rafaverde.png"
-          alt="Profile Avatar"
-          className="w-20 rounded-full"
-        />
+          <div>
+            <div className="mb-2 flex items-center justify-between">
+              <h2 className="m-0 text-2xl leading-0 font-medium text-emerald-500">
+                Rafael Valverde
+              </h2>
+              <a href="" target="_blank">
+                <Link className="m-0 text-sm text-zinc-400" size={20} />
+              </a>
+            </div>
 
-        <div>
-          <h2 className="text-2xl font-medium text-purple-500">
-            Rafael Valverde
-          </h2>
+            <div className="mb-3 flex gap-6">
+              <a href="" target="_blank">
+                <div className="flex items-center gap-2">
+                  <UsersRound size={18} className="text-emerald-600" />
+                  <span className="text-sm text-zinc-500">4 followers</span>
+                </div>
+              </a>
+
+              <a href="" target="_blank">
+                <div className="flex items-center gap-2">
+                  <BookMarked size={18} className="text-emerald-600" />
+                  <span className="text-sm text-zinc-500">36 repos</span>
+                </div>
+              </a>
+            </div>
+
+            <p className="line text-sm leading-6 text-zinc-400">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Repellendus officia quidem itaque similique dolor cumque eius
+              necessitatibus alias temporibus aliquid! Expedita ipsam ipsum non,
+              architecto inventore totam. Id, praesentium ad.
+            </p>
+          </div>
         </div>
       </div>
     </div>
